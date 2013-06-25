c9af3899b0d750696e020b4c553104c38e8fa11f
ThiagoAugustus <ThiagoAugustus@Note>
2013-06-25 15:06:09 -0300

    Primeiro commit sistema

---
+++ src/br/edu/unifil/config/Connection.java
@@ -0,0 +1,5 @@
+package br.edu.unifil.config;
+
+public class Connection {
+    
+}
---
+++ src/br/edu/unifil/hotel/Main.java
@@ -0,0 +1,8 @@
+package br.edu.unifil.hotel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
